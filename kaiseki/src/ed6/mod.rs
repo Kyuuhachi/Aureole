4483290079ade7585dfa688078bde7db4465c358
Kyuuhachi <caagr98@gmail.com>
2022-04-20 20:53:35 +0200

    Inline ed6/mod.rs

--- kaiseki/src/ed6/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod archive;
-pub mod magic;
-pub use archive::{Archive,Archives};
--- kaiseki/src/lib.rs
+++ kaiseki/src/lib.rs
@@ -1,3 +1,7 @@
-pub mod ed6;
+pub mod ed6 {
+	pub mod archive;
+	pub mod magic;
+	pub use archive::{Archive,Archives};
+}
 mod decompress;
 mod util;

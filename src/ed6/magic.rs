a8e8a5d5df74f08f18576425ff71bdc20a3b6e71
Kyuuhachi <caagr98@gmail.com>
2022-04-20 15:19:37 +0200

    Move magic render to separate module

---
+++ src/ed6/magic.rs
@@ -0,0 +1,122 @@
+use kaiseki::ed6::magic::*;
+
+pub fn render(magics: &Vec<Magic<String>>) -> choubun::Node {
+	choubun::document(|doc| {
+		doc.root.attr("lang", "en");
+		doc.head.node("title", |a| a.text("Arts list"));
+		doc.head.node("style", |a| a.raw(r#"
+			table { border-collapse: collapse; }
+			tr { border-bottom: 1px solid #7F7F7F3F; }
+			td { padding-left: 1ex; padding-right: 1ex; }
+			[title] { text-decoration: underline dotted; }
+
+			.el-none   { background-color: #7F7F7F3F; }
+			.el-earth  { background-color: #CC88553F; }
+			.el-water  { background-color: #3366FF3F; }
+			.el-fire   { background-color: #FF33333F; }
+			.el-wind   { background-color: #3399333F; }
+			.el-time   { background-color: #6677883F; }
+			.el-space  { background-color: #FFEE773F; }
+			.el-mirage { background-color: #CCCCCC3F; }
+		"#));
+
+		doc.body.node("table", |a| {
+			a.indent();
+			for Magic {
+				id, name, desc,
+				flags, element,
+				target, effect1, effect2,
+				target_p1, target_p2,
+				warmup, cooldown, cost, sort,
+				effect_p1, effect_p2, effect_p3, effect_p4,
+			} in magics {
+				a.node("tr", |a| {
+					a.indent();
+					a.class(format!("el-{}", element.to_string().to_lowercase()));
+
+					a.node("td", |a| {
+						a.class("name");
+						a.node("small", |a| a.text(id.to_string()));
+						a.text(" ");
+						a.node("span", |a| {
+							if !desc.trim().is_empty() {
+								a.attr("title", desc.replace("\\n", "\n"));
+							}
+							a.text(name);
+						});
+					});
+
+					a.node("td", |a| {
+						a.node("span", |a| a.text(sort.to_string()));
+					});
+
+					a.node("td", |a| {
+						a.class("target");
+						a.text(format!("{target}({target_p1}, {target_p2})"));
+					});
+
+					if (*effect2, *effect_p3, *effect_p4) == (MagicEffect::None, 0, 0) {
+						a.node("td", |a| {
+							a.class("effect");
+							a.attr("colspan", "2");
+							a.text(format!("{effect1}({effect_p1}, {effect_p2})"));
+						});
+					} else if *effect2 == MagicEffect::None {
+						a.node("td", |a| {
+							a.class("effect");
+							a.attr("colspan", "2");
+							a.text(format!("{effect1}({effect_p1}, {effect_p2}, {effect_p3}, {effect_p4})"));
+						});
+					} else {
+						a.node("td", |a| {
+							a.class("effect");
+							a.text(format!("{effect1}({effect_p1}, {effect_p2})"));
+						});
+						a.node("td", |a| {
+							a.class("effect");
+							a.text(format!("{effect2}({effect_p3}, {effect_p4})"));
+						});
+					}
+
+					a.node("td", |a| {
+						a.class("flags");
+						for (i, c) in "涙本..敵死良友あい砲.う呪え.".chars().enumerate() {
+							let i = match MagicFlags::from_bits(1<<i) {
+								Some(i) => i,
+								None => continue
+							};
+							if flags.contains(i) {
+								a.node("span", |a| {
+									a.attr("title", format!("{:?}", i));
+									a.text(format!("{}", c));
+								});
+							} else {
+								a.text("・");
+							}
+						}
+					});
+
+					a.node("td", |a| {
+						a.class("cost");
+						if *cost == 0 {
+							// empty
+						} else if flags.contains(MagicFlags::Magic) {
+							a.text(format!("{} EP", cost));
+						} else {
+							a.text(format!("{} CP", cost));
+						}
+					});
+
+					a.node("td", |a| {
+						a.class("time");
+						if *warmup != 0 {
+							a.text(format!("{warmup}+{cooldown} T"));
+						} else if *cooldown != 0 {
+							a.text(format!("{cooldown} T"));
+						}
+					});
+				});
+			}
+		});
+	})
+}
--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,20 @@
 use anyhow::Result;
-use kaiseki::ed6;
+use kaiseki::ed6::Archives;
 use kaiseki::ed6::magic::*;
 use rocket::State;
 use rocket::response::content::Html;
 
+pub mod ed6 {
+	pub mod magic;
+}
+
 type HtmlOut = Result<Html<String>, rocket::response::Debug<anyhow::Error>>;
 
 #[rocket::get("/fc/magic")]
-fn fc_magic(arch: &State<ed6::Archives>) -> HtmlOut {
+fn fc_magic(arch: &State<Archives>) -> HtmlOut {
 	let data = arch.get_compressed_by_name(0x2, *b"T_MAGIC ._DT")?.1;
-
 	let magics = Magic::read(&data)?;
-
-	let doc = choubun::document(|doc| {
-		doc.root.attr("lang", "en");
-		doc.head.node("title", |a| a.text("Arts list"));
-		doc.head.node("style", |a| a.raw(r#"
-			table { border-collapse: collapse; }
-			tr { border-bottom: 1px solid #7F7F7F3F; }
-			td { padding-left: 1ex; padding-right: 1ex; }
-			[title] { text-decoration: underline dotted; }
-
-			.el-none   { background-color: #7F7F7F3F; }
-			.el-earth  { background-color: #CC88553F; }
-			.el-water  { background-color: #3366FF3F; }
-			.el-fire   { background-color: #FF33333F; }
-			.el-wind   { background-color: #3399333F; }
-			.el-time   { background-color: #6677883F; }
-			.el-space  { background-color: #FFEE773F; }
-			.el-mirage { background-color: #CCCCCC3F; }
-		"#));
-
-		doc.body.node("table", |a| {
-			a.indent();
-			for Magic {
-				id, name, desc,
-				flags, element,
-				target, effect1, effect2,
-				target_p1, target_p2,
-				warmup, cooldown, cost, sort,
-				effect_p1, effect_p2, effect_p3, effect_p4,
-			} in magics {
-				a.node("tr", |a| {
-					a.indent();
-					a.class(format!("el-{}", element.to_string().to_lowercase()));
-
-					a.node("td", |a| {
-						a.class("name");
-						a.node("small", |a| a.text(id.to_string()));
-						a.text(" ");
-						a.node("span", |a| {
-							if !desc.trim().is_empty() {
-								a.attr("title", desc.replace("\\n", "\n"));
-							}
-							a.text(name);
-						});
-					});
-
-					a.node("td", |a| {
-						a.node("span", |a| a.text(sort.to_string()));
-					});
-
-					a.node("td", |a| {
-						a.class("target");
-						a.text(format!("{target}({target_p1}, {target_p2})"));
-					});
-
-					if (effect2, effect_p3, effect_p4) == (MagicEffect::None, 0, 0) {
-						a.node("td", |a| {
-							a.class("effect");
-							a.attr("colspan", "2");
-							a.text(format!("{effect1}({effect_p1}, {effect_p2})"));
-						});
-					} else if effect2 == MagicEffect::None {
-						a.node("td", |a| {
-							a.class("effect");
-							a.attr("colspan", "2");
-							a.text(format!("{effect1}({effect_p1}, {effect_p2}, {effect_p3}, {effect_p4})"));
-						});
-					} else {
-						a.node("td", |a| {
-							a.class("effect");
-							a.text(format!("{effect1}({effect_p1}, {effect_p2})"));
-						});
-						a.node("td", |a| {
-							a.class("effect");
-							a.text(format!("{effect2}({effect_p3}, {effect_p4})"));
-						});
-					}
-
-					a.node("td", |a| {
-						a.class("flags");
-						for (i, c) in "涙本..敵死良友あい砲.う呪え.".chars().enumerate() {
-							let i = match MagicFlags::from_bits(1<<i) {
-								Some(i) => i,
-								None => continue
-							};
-							if flags.contains(i) {
-								a.node("span", |a| {
-									a.attr("title", format!("{:?}", i));
-									a.text(format!("{}", c));
-								});
-							} else {
-								a.text("・");
-							}
-						}
-					});
-
-					a.node("td", |a| {
-						a.class("cost");
-						if cost == 0 {
-							// empty
-						} else if flags.contains(MagicFlags::Magic) {
-							a.text(format!("{} EP", cost));
-						} else {
-							a.text(format!("{} CP", cost));
-						}
-					});
-
-					a.node("td", |a| {
-						a.class("time");
-						if warmup != 0 {
-							a.text(format!("{warmup}+{cooldown} T"));
-						} else if cooldown != 0 {
-							a.text(format!("{cooldown} T"));
-						}
-					});
-				});
-			}
-		});
-	});
+	let doc = ed6::magic::render(&magics);
 	let mut s = String::new();
 	doc.render(&mut s).unwrap();
 	Ok(Html(s))
@@ -138,6 +23,6 @@ fn fc_magic(arch: &State<ed6::Archives>) -> HtmlOut {
 #[rocket::launch]
 fn rocket() -> _ {
 	rocket::build()
-		.manage(ed6::Archives::new("data/fc"))
+		.manage(Archives::new("data/fc"))
 		.mount("/", rocket::routes![fc_magic])
 }

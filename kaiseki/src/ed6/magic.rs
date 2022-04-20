5a8f4710ff7fb758aaeb4f9aecb3c2d450131197
Kyuuhachi <caagr98@gmail.com>
2022-04-20 21:23:11 +0200

    self goes first

--- kaiseki/src/ed6/magic.rs
+++ kaiseki/src/ed6/magic.rs
@@ -1,7 +1,7 @@
 use int_enum::IntEnum;
 use anyhow::{Result, Context};
 use hamu::read::{In, Le};
-use crate::util::{In_Ext, self};
+use crate::util::{self, In_Ext};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, strum_macros::Display)]
 pub enum Element {

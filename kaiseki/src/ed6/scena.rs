e7fb674faf672a92eb737104673edbc7340ee317
Kyuuhachi <caagr98@gmail.com>
2022-04-22 13:58:44 +0200

    Fill out more insns
    Can now parse R0100 and R0101

--- kaiseki/src/ed6/scena.rs
+++ kaiseki/src/ed6/scena.rs
@@ -284,9 +284,9 @@ pub enum Expr {
 	Unop(ExprUnop, Box<Expr>),
 	Exec(Insn),
 	Flag(Flag),
-	Var(u16),
-	Attr(u8),
-	CharAttr(Character, u8),
+	Var(u16 /*Var*/),
+	Attr(u8 /*Attr*/),
+	CharAttr(Character, u8 /*CharAttr*/),
 	Rand,
 }
 
@@ -301,34 +301,55 @@ pub enum Insn {
 	/*0A*/ FlagsUnset(u32 /*Flags*/),
 	/*0B*/ FadeOn(u32 /*Time*/, u32 /*Color*/, u8),
 	/*0C*/ FadeOff(u32 /*Time*/, u32 /*Color*/),
+	/*0D*/ _0D,
+	/*0F*/ Battle(u16 /*BattleId*/, u16, u16, u16, u8, u16, i8),
 	/*16*/ Map(MapInsn),
 	/*19*/ EventBegin(u8),
 	/*1A*/ EventEnd(u8),
 	/*1B*/ _1B(u16, u16),
 	/*1C*/ _1C(u16, u16),
+	/*22*/ SoundPlay(u16 /*Sound*/, u8, u8 /*Volume*/),
+	/*23*/ SoundStop(u16 /*Sound*/),
+	/*24*/ SoundLoop(u16 /*Sound*/, u8),
+	/*28*/ Quest(u16 /*Quest*/, QuestInsn),
+	/*29*/ QuestGet(u16 /*Quest*/, QuestGetInsn),
+	/*30*/ _Party30(u8),
 	/*43*/ CharForkFunc(Character, u8 /*ForkId*/, FuncRef),
 	/*45*/ CharFork(Character, u16 /*ForkId*/, Vec<Insn>), // why is this is u16?
 	/*49*/ Event(FuncRef), // Not sure if this is different from Call
+	/*4D*/ ExprVar(u16 /*Var*/, Box<Expr>),
+	/*4F*/ ExprAttr(u8 /*Attr*/, Box<Expr>),
+	/*51*/ ExprCharAttr(Character, u8 /*CharAttr*/, Box<Expr>),
 	/*53*/ TextEnd(Character),
 	/*54*/ TextMessage(util::Text),
-	/*56*/ TextReset(u8), // Not sure what this does, is it always 0?
+	/*56*/ TextReset(u8),
 	/*58*/ TextWait,
 	/*5A*/ TextSetPos(i16, i16, i16, i16),
 	/*5B*/ TextTalk(Character, util::Text),
 	/*5C*/ TextTalkNamed(Character, String, util::Text),
+	/*5D*/ Menu(u16 /*MenuId*/, (i16, i16) /*Pos*/, u8, Vec<String>),
+	/*5E*/ MenuWait(u16 /*MenuId*/),
+	/*5F*/ _Menu5F(u16 /*MenuId*/), // MenuClose?
 	/*60*/ TextSetName(String),
+	/*69*/ CamLookAt(Character, u32 /*Time*/),
 	/*6C*/ CamAngle(i32 /*Angle*/, u32 /*Time*/),
 	/*6D*/ CamPos(Pos3, u32 /*Time*/),
-	/*88*/ CharSetPos(Character, Pos3, u16),
+	/*87*/ CharSetFrame(Character, u16),
+	/*88*/ CharSetPos(Character, Pos3, u16 /*Angle*/),
 	/*8A*/ CharLookAt(Character, Character, u16 /*Time*/),
 	/*8E*/ CharWalkTo(Character, Pos3, u32 /*Speed*/, u8),
 	/*90*/ CharWalk(Character, Pos3, u32 /*Speed*/, u8), // I don't know how this differs from CharWalkTo; is it relative maybe?
 	/*92*/ _Char92(Character, Character, u32, u32, u8),
-	/*A2*/ FlagSet(Flag), // Is this order really right?
+	/*99*/ CharAnimation(Character, u8, u8, u32 /*Time*/),
+	/*9A*/ CharFlagsSet(Character, u16 /*CharFlags*/),
+	/*9B*/ CharFlagsUnset(Character, u16 /*CharFlags*/),
+	/*A2*/ FlagSet(Flag),
 	/*A3*/ FlagUnset(Flag),
 	/*A5*/ AwaitFlagUnset(Flag),
 	/*A6*/ AwaitFlagSet(Flag),
 	/*B1*/ OpLoad(String /*._OP filename*/),
+	/*B2*/ _B2(u8, u8, u16),
+	/*B4*/ ReturnToTitle(u8),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -336,6 +357,21 @@ pub enum MapInsn {
 	/*00*/ Hide,
 	/*01*/ Show,
 	/*02*/ Set(i32, (i32, i32), FileRef /* archive 03 */), // XXX this seems to be (arch, index) while others are (index, arch)?
+}
+
+// I am unsure whether these are Set or Unset
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum QuestInsn {
+	/*01*/ TaskSet(u16),
+	/*02*/ TaskUnset(u16),
+	/*03*/ FlagsSet(u8),
+	/*04*/ FlagsUnset(u8),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum QuestGetInsn {
+	/*00*/ Task(u16),
+	/*01*/ Flags(u8),
 }
 
 struct FuncParser {
@@ -400,16 +436,34 @@ impl FuncParser {
 			0x0A => Insn::FlagsUnset(i.u32()?),
 			0x0B => Insn::FadeOn(i.u32()?, i.u32()?, i.u8()?),
 			0x0C => Insn::FadeOff(i.u32()?, i.u32()?),
+			0x0D => Insn::_0D,
+			0x0F => Insn::Battle(i.u16()?, i.u16()?, i.u16()?, i.u16()?, i.u8()?, i.u16()?, i.i8()?),
 			0x16 => Insn::Map(match i.u8()? {
 				0x00 => MapInsn::Hide,
 				0x01 => MapInsn::Show,
 				0x02 => MapInsn::Set(i.i32()?, (i.i32()?, i.i32()?), i.file_ref()?),
-				op => eyre::bail!("Unknown map op: {:02X}", op)
+				op => eyre::bail!("Unknown MapInsn: {:02X}", op)
 			}),
 			0x19 => Insn::EventBegin(i.u8()?),
 			0x1A => Insn::EventEnd(i.u8()?),
 			0x1B => Insn::_1B(i.u16()?, i.u16()?),
 			0x1C => Insn::_1C(i.u16()?, i.u16()?),
+			0x22 => Insn::SoundPlay(i.u16()?, i.u8()?, i.u8()?),
+			0x23 => Insn::SoundStop(i.u16()?),
+			0x24 => Insn::SoundLoop(i.u16()?, i.u8()?),
+			0x28 => Insn::Quest(i.u16()?, match i.u8()? {
+				0x01 => QuestInsn::TaskSet(i.u16()?),
+				0x02 => QuestInsn::TaskUnset(i.u16()?),
+				0x03 => QuestInsn::FlagsSet(i.u8()?),
+				0x04 => QuestInsn::FlagsUnset(i.u8()?),
+				op => eyre::bail!("Unknown QuestInsn: {:02X}", op)
+			}),
+			0x29 => Insn::QuestGet(i.u16()?, match i.u8()? {
+				0x00 => QuestGetInsn::Task(i.u16()?),
+				0x01 => QuestGetInsn::Flags(i.u8()?),
+				op => eyre::bail!("Unknown QuestGetInsn: {:02X}", op)
+			}),
+			0x30 => Insn::_Party30(i.u8()?),
 			0x43 => Insn::CharForkFunc(Character(i.u16()?), i.u8()?, FuncRef(i.u8()? as u16, i.u16()?)),
 			0x45 => Insn::CharFork(Character(i.u16()?), i.u16()?, {
 				let end = i.u8()? as usize + i.pos();
@@ -423,6 +477,9 @@ impl FuncParser {
 				insns
 			}),
 			0x49 => Insn::Event(FuncRef(i.u8()? as u16, i.u16()?)),
+			0x4D => Insn::ExprVar(i.u16()?, self.read_expr(i)?),
+			0x4F => Insn::ExprAttr(i.u8()?, self.read_expr(i)?),
+			0x51 => Insn::ExprCharAttr(Character(i.u16()?), i.u8()?, self.read_expr(i)?),
 			0x53 => Insn::TextEnd(Character(i.u16()?)),
 			0x54 => Insn::TextMessage(self.read_text(i)?),
 			0x56 => Insn::TextReset(i.u8()?),
@@ -430,21 +487,31 @@ impl FuncParser {
 			0x5A => Insn::TextSetPos(i.i16()?, i.i16()?, i.i16()?, i.i16()?),
 			0x5B => Insn::TextTalk(Character(i.u16()?), self.read_text(i)?),
 			0x5C => Insn::TextTalkNamed(Character(i.u16()?), i.str()?, self.read_text(i)?),
+			0x5D => Insn::Menu(i.u16()?, (i.i16()?, i.i16()?), i.u8()?, i.str()?.split_terminator('\x01').map(|a| a.to_owned()).collect()),
+			0x5E => Insn::MenuWait(i.u16()?),
+			0x5F => Insn::_Menu5F(i.u16()?),
 			0x60 => Insn::TextSetName(i.str()?),
+			0x69 => Insn::CamLookAt(Character(i.u16()?), i.u32()?),
 			0x6C => Insn::CamAngle(i.i32()?, i.u32()?),
 			0x6D => Insn::CamPos(i.pos3()?, i.u32()?),
+			0x87 => Insn::CharSetFrame(Character(i.u16()?), i.u16()?),
 			0x88 => Insn::CharSetPos(Character(i.u16()?), i.pos3()?, i.u16()?),
 			0x8A => Insn::CharLookAt(Character(i.u16()?), Character(i.u16()?), i.u16()?),
 			0x8E => Insn::CharWalkTo(Character(i.u16()?), i.pos3()?, i.u32()?, i.u8()?),
 			0x90 => Insn::CharWalk(Character(i.u16()?), i.pos3()?, i.u32()?, i.u8()?),
 			0x92 => Insn::_Char92(Character(i.u16()?), Character(i.u16()?), i.u32()?, i.u32()?, i.u8()?),
+			0x99 => Insn::CharAnimation(Character(i.u16()?), i.u8()?, i.u8()?, i.u32()?),
+			0x9A => Insn::CharFlagsSet(Character(i.u16()?), i.u16()?),
+			0x9B => Insn::CharFlagsUnset(Character(i.u16()?), i.u16()?),
 			0xA2 => Insn::FlagSet(Flag(i.u16()?)),
 			0xA3 => Insn::FlagUnset(Flag(i.u16()?)),
 			0xA5 => Insn::AwaitFlagUnset(Flag(i.u16()?)),
 			0xA6 => Insn::AwaitFlagSet(Flag(i.u16()?)),
 			0xB1 => Insn::OpLoad(i.str()?),
+			0xB2 => Insn::_B2(i.u8()?, i.u8()?, i.u16()?),
+			0xB4 => Insn::ReturnToTitle(i.u8()?),
 
-			op => eyre::bail!("Unknown op: {:02X}", op)
+			op => eyre::bail!("Unknown Insn: {:02X}", op)
 		})
 	}
 
@@ -507,7 +574,7 @@ impl FuncParser {
 				0x20 => Expr::Attr(i.u8()?),
 				0x21 => Expr::CharAttr(Character(i.u16()?), i.u8()?),
 				0x22 => Expr::Rand,
-				op => eyre::bail!("Unknown expr op: {:02X}", op)
+				op => eyre::bail!("Unknown Expr: {:02X}", op)
 			};
 			stack.push(op);
 			self.marks.insert(i.pos(), "\x1B[0;7;2m•".to_owned());
--- kaiseki/src/util.rs
+++ kaiseki/src/util.rs
@@ -169,14 +169,14 @@ pub fn read_text(i: &mut In) -> Result<Text> {
 		// 0x09 =>
 		// 0x18 =>
 		0x1F => { drain(&mut segments, &mut curr)?; segments.push(TextSegment::Item(i.u16()?)) }
-		op@(0x00..=0x1F) => eyre::bail!("Invalid text op: {:02X}", op),
+		op@(0x00..=0x1F) => eyre::bail!("Invalid TextSegment: b{:?}", char::from(op)),
 		b'#' => {
 			drain(&mut segments, &mut curr)?;
 			let mut n = 0;
 			loop { match i.u8()? {
 				ch@(b'0'..=b'9') => n = n * 10 + (ch - b'0') as u16,
 				b'F' => { segments.push(TextSegment::Face(n)); break },
-				op => eyre::bail!("Invalid text op: #{}{}", n, op),
+				op => eyre::bail!("Invalid TextSegment: #{}{}", n, op),
 			} }
 		}
 		ch => {

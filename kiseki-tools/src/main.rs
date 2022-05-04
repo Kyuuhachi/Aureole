fbcd39332b947161b8198638cc8884de6c894dd7
Kyuuhachi <caagr98@gmail.com>
2022-05-04 21:44:38 +0200

    Add extract-all command

--- kiseki-tools/src/main.rs
+++ kiseki-tools/src/main.rs
@@ -6,8 +6,6 @@ use clap::{StructOpt, IntoApp};
 use kaiseki::ed6::Archive;
 
 #[derive(Debug, Clone, clap::Parser)]
-#[clap(author, version, about, long_about = None)]
-#[clap(propagate_version = true)]
 struct Cli {
 	#[clap(subcommand)]
 	command: Command,
@@ -15,6 +13,7 @@ struct Cli {
 
 #[derive(Debug, Clone, clap::Subcommand)]
 enum Command {
+	/// Extract a single .dir/.dat archive.
 	Extract {
 		/// Overwrite the output directory if it already exists.
 		#[clap(short, long)]
@@ -23,7 +22,23 @@ enum Command {
 		/// .dir file to read from. Corresponding .dat file must also exist.
 		#[clap(value_hint=clap::ValueHint::FilePath)]
 		dirfile: PathBuf,
-		/// Directory to write files to.
+		/// Directory to write extracted files to.
+		#[clap(value_hint=clap::ValueHint::DirPath)]
+		outdir: PathBuf,
+	},
+
+	/// Extract multiple .dir/.dat archives from a directory.
+	///
+	/// Each `<indir>/{file}.dir` will be extracted to `<outdir>/{file}`.
+	ExtractAll {
+		/// Overwrite the output directories if they already exist.
+		#[clap(short, long)]
+		force: bool,
+
+		/// Directory containing .dir files to be extracted.
+		#[clap(value_hint=clap::ValueHint::DirPath)]
+		indir: PathBuf,
+		/// Superdirectory to write extracted directories to.
 		#[clap(value_hint=clap::ValueHint::DirPath)]
 		outdir: PathBuf,
 	},
@@ -34,7 +49,7 @@ fn main() -> eyre::Result<()> {
 	let mut cmd = Cli::command();
 	match cli.command {
 		Command::Extract { force, dirfile, outdir } => {
-			if !dirfile.is_file() || dirfile.extension().filter(|a| "dir" == *a).is_none() {
+			if !dirfile.is_file() || dirfile.extension().filter(|a| a == &"dir").is_none() {
 				cmd.error(clap::ErrorKind::ValueValidation, "Input must be a .dir file").exit();
 			}
 			let datfile = dirfile.with_extension("dat");
@@ -52,6 +67,35 @@ fn main() -> eyre::Result<()> {
 
 			extract(&dirfile, &datfile, &outdir)?;
 		},
+
+		Command::ExtractAll { force, indir, outdir } => {
+			if !indir.is_dir() {
+				cmd.error(clap::ErrorKind::ValueValidation, "Invalid input directory").exit();
+			}
+
+			for a in fs::read_dir(&indir)? {
+				let a = a?;
+				let dirfile = a.path();
+				if dirfile.extension().filter(|a| a == &"dir").is_some() {
+					let datfile = dirfile.with_extension("dat");
+					if !datfile.is_file() {
+						cmd.error(clap::ErrorKind::ValueValidation, ".dat file not found").exit();
+					}
+
+					let outdir = outdir.join(dirfile.file_stem().unwrap());
+
+					if outdir.exists() {
+						if force {
+							fs::remove_dir_all(&outdir)?;
+						} else {
+							cmd.error(clap::ErrorKind::ValueValidation, "Output directory already exists (use -f to overwrite)").exit();
+						}
+					}
+
+					extract(&dirfile, &datfile, &outdir)?;
+				}
+			}
+		},
 	}
 
 	Ok(())
@@ -65,25 +109,35 @@ fn extract(dirfile: &PathBuf, datfile: &PathBuf, outdir: &PathBuf) -> eyre::Resu
 
 	for e in arch.entries() {
 		let outfile = outdir.join(e.display_name());
-		println!("Extracting {}", outfile.display());
+		let data = arch.get(e.index).unwrap().1;
+		println!("Extracting {} ({} → {})", outfile.display(), data.len(), e.size);
+
 		let note = if e.timestamp == 0 {
 			" e "
 		} else {
 			let time = filetime::FileTime::from_unix_time(e.timestamp as i64, 0);
-			let data = arch.get(e.index).unwrap().1;
 			if data.len() == e.size {
 				fs::write(&outfile, data)?;
 				filetime::set_file_mtime(&outfile, time)?;
 				"   "
 			} else {
-				let data = kaiseki::decompress::decompress(data).unwrap();
-				fs::write(&outfile, data)?;
-				filetime::set_file_mtime(&outfile, time)?;
-				"(C)"
+				match kaiseki::decompress::decompress(data) {
+					Ok(udata) => {
+						fs::write(&outfile, udata)?;
+						filetime::set_file_mtime(&outfile, time)?;
+						"(C)"
+					}
+					Err(e) => {
+						println!("  Decompression failed: {}", e);
+						fs::write(&outfile, data)?;
+						filetime::set_file_mtime(&outfile, time)?;
+						"(?)"
+					}
+				}
 			}
 		};
 
-		writeln!(index, "{:4} {} {:?}", e.index, note, e.name)?;
+		writeln!(index, "{:4} {} {:?} ({} → {})", e.index, note, e.name, data.len(), e.size)?;
 	}
 
 	Ok(())

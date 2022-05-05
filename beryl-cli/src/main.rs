5a5b2d2d9d904ab097a59729bb570fc28f1a4e9b
Kyuuhachi <caagr98@gmail.com>
2022-05-05 16:33:46 +0200

    Add Beryl, a hex dump tool
    Will integrate it into Hamu soon

---
+++ beryl-cli/src/main.rs
@@ -0,0 +1,83 @@
+use std::{
+	io::{self, prelude::*},
+	path::PathBuf,
+	ffi::OsStr,
+};
+use clap::StructOpt;
+
+#[derive(Debug, Clone, clap::Parser)]
+#[clap(name = "beryl", version, setting(clap::AppSettings::DeriveDisplayOrder))]
+struct Cli {
+	#[clap(long, short, parse(try_from_str = parse_size), default_value="0", hide_default_value=true)]
+	start: usize,
+
+	#[clap(long, short, parse(try_from_str = parse_size), conflicts_with_all = &["length", "lines"])]
+	end: Option<usize>,
+
+	#[clap(long, short, parse(try_from_str = parse_size))]
+	length: Option<usize>,
+
+	#[clap(long, short='L')]
+	lines: Option<usize>,
+
+	#[clap(long, short, parse(try_from_str = parse_size))]
+	width: Option<usize>,
+
+	#[clap(long, short='N', value_name="DIGITS")]
+	num_width: Option<usize>,
+
+	#[clap(long, short='n')]
+	no_blank: bool,
+
+	#[clap(long, short='1')]
+	one_line: bool,
+
+	#[clap(long)]
+	gray: bool,
+
+	#[clap(value_hint=clap::ValueHint::FilePath)]
+	files: Vec<PathBuf>,
+}
+
+fn parse_size(s: &str) -> Result<usize, std::num::ParseIntError> {
+	if s.starts_with("0x") {
+		usize::from_str_radix(s.trim_start_matches("0x"), 16)
+	} else if s.starts_with("0o") {
+		usize::from_str_radix(s.trim_start_matches("0o"), 8)
+	} else if s.starts_with("0b") {
+		usize::from_str_radix(s.trim_start_matches("0b"), 2)
+	} else {
+		s.parse::<usize>()
+	}
+}
+
+fn main() -> io::Result<()> {
+	let cli = Cli::parse();
+	let files = if cli.files.is_empty() {
+		vec![PathBuf::from("-")]
+	} else {
+		cli.files
+	};
+	for file in files {
+		let mut file: Box<dyn io::Read> = if file == OsStr::new("-") {
+			Box::new(io::stdin())
+		} else {
+			Box::new(std::fs::File::open(file)?)
+		};
+
+		io::copy(&mut (&mut file).take(cli.start as u64), &mut io::sink())?;
+
+		let mut dump = beryl::Dump::new(&mut file, cli.start);
+		if let Some(v) = cli.end    { dump = dump.end(v); }
+		if let Some(v) = cli.lines  { dump = dump.lines(v); }
+		if let Some(v) = cli.length { dump = dump.bytes(v); }
+		if let Some(v) = cli.width  { dump = dump.end(v); }
+		if let Some(v) = cli.num_width  { dump = dump.num_width(v); }
+		if cli.no_blank  { dump = dump.newline(false); }
+		if cli.one_line  { dump = dump.oneline(); }
+		if cli.gray  { dump = dump.color(&beryl::color::gray); }
+
+		dump.to_stdout();
+	}
+	Ok(())
+}
---
+++ beryl/src/lib.rs
@@ -0,0 +1,237 @@
+use std::{
+	collections::BTreeMap,
+	io,
+};
+pub use ansi_term::{Color::Fixed as Color, Style, ANSIString as Ansi};
+
+pub mod preview {
+	use super::*;
+
+	pub fn ascii(f: &mut Vec<Ansi>, data: &[u8]) {
+		for b in data {
+			f.push(match b {
+				0x20..=0x7E => (*b as char).to_string().into(),
+				_           => Style::new().dimmed().paint("·"),
+			})
+		}
+	}
+}
+
+pub mod color {
+	use super::*;
+
+	pub fn ascii(byte: u8) -> Style {
+		match byte {
+			0x00        => Style::new().dimmed(),
+			0xFF        => Style::new().fg(Color(9)), // bright red
+			0x20..=0x7E => Style::new().fg(Color(10)), // bright green
+			_           => Style::new(),
+		}
+	}
+
+	pub fn gray(byte: u8) -> Style {
+		Style::new()
+			.fg(Color(if byte <  0x30 { 245 } else { 236 }))
+			.on(Color(if byte == 0x00 { 237 } else { 238 + byte / 16 }))
+	}
+
+	pub fn none(_byte: u8) -> Style { Style::new() }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum DumpLength {
+	None,
+	Bytes(usize),
+	Lines(usize),
+}
+
+#[must_use]
+pub struct Dump<'a> {
+	reader: &'a mut dyn io::Read,
+	start: usize,
+	length: DumpLength,
+	width: usize,
+	color: &'a dyn Fn(u8) -> Style,
+	preview: Option<&'a dyn Fn(&mut Vec<Ansi>, &[u8])>,
+	num_width: usize,
+	newline: bool,
+	marks: BTreeMap<usize, Ansi<'a>>,
+}
+
+impl<'a> Dump<'a> {
+	pub fn new(reader: &'a mut dyn io::Read, start: usize) -> Self {
+		Self {
+			reader,
+			start,
+			width: 0,
+			length: DumpLength::None,
+			color: &color::ascii,
+			preview: Some(&preview::ascii),
+			num_width: 1,
+			newline: true,
+			marks: BTreeMap::new(),
+		}
+	}
+
+	pub fn oneline(self) -> Self {
+		self.lines(1).newline(false)
+	}
+
+	pub fn lines(self, lines: usize) -> Self {
+		Dump { length: DumpLength::Lines(lines), ..self }
+	}
+
+	pub fn bytes(self, bytes: usize) -> Self {
+		Dump { length: DumpLength::Bytes(bytes), ..self }
+	}
+
+	pub fn end(self, end: usize) -> Self {
+		Dump { length: DumpLength::Bytes(end - self.start), ..self }
+	}
+
+	pub fn width(self, width: usize) -> Self {
+		assert!(width > 0);
+		Dump { width, ..self }
+	}
+
+	pub fn color(self, color: &'a dyn Fn(u8) -> Style) -> Self {
+		Dump { color, ..self }
+	}
+
+	pub fn preview(self, preview: &'a dyn Fn(&mut Vec<Ansi>, &[u8])) -> Self {
+		Dump { preview: Some(preview), ..self }
+	}
+
+	// This is different from a no-op preview function, since it affects the
+	// default width and trailing space on the last line.
+	pub fn no_preview(self) -> Self {
+		Dump { preview: None, ..self }
+	}
+
+	pub fn num_width(self, num_width: usize) -> Self {
+		Dump { num_width, ..self }
+	}
+
+	pub fn newline(self, newline: bool) -> Self {
+		Dump { newline, ..self }
+	}
+
+	pub fn mark(mut self, pos: usize, mark: impl Into<Ansi<'a>>) -> Self {
+		self.marks.insert(pos, mark.into());
+		self
+	}
+
+	pub fn marks(mut self, iter: impl Iterator<Item=(usize, impl Into<Ansi<'a>>)>) -> Self {
+		for (k, v) in iter {
+			self = self.mark(k, v);
+		}
+		self
+	}
+
+	pub fn write_to(self, out: &mut impl io::Write, ansi: bool) -> io::Result<()> {
+		let width = match self.width {
+			0 if self.preview.is_some() => 48,
+			0 if self.preview.is_none() => 72,
+			w => w,
+		};
+
+		let len = match self.length {
+			DumpLength::None => None,
+			DumpLength::Bytes(b) => Some(b),
+			DumpLength::Lines(l) => Some(l * width),
+		};
+
+		let mut marks = self.marks.range(self.start..).peekable();
+		let mut pos = self.start;
+		let mut buf = vec![0; width];
+
+		let num_width = if let Some(len) = len {
+			self.num_width.max(format!("{:X}", self.start + len).len())
+		} else {
+			self.num_width
+		};
+
+		loop {
+			let buf = if let Some(len) = len {
+				&mut buf[..width.min(self.start + len - pos)]
+			} else {
+				&mut buf[..width]
+			};
+			let nread = self.reader.read(buf)?;
+			let buf = &buf[..nread];
+
+
+			let mut line = Vec::new();
+			if self.num_width > 0 {
+				let s = format!("{:X}", pos);
+				let style = Style::new().fg(Color(3));
+				if s.len() < num_width {
+					line.push(style.dimmed().paint("0".repeat(num_width - s.len())));
+				}
+				line.push(style.paint(s));
+			}
+
+			for &b in buf {
+				match marks.next_if(|&(&a, _)| a <= pos) {
+					Some((_, mark)) => line.push(mark.clone()), // cloning here is a little wasteful
+					_ => line.push(" ".into()),
+				}
+				let style = (self.color)(b);
+				line.push(style.paint(format!("{:02X}", b)));
+				pos += 1;
+			}
+			// If a mark lands on a line break, we'll print it both before and after because why not.
+			match marks.peek() {
+				Some(&(&a, mark)) if a <= pos => line.push(mark.clone()),
+				_ => line.push(" ".into()),
+			}
+
+			if let Some(preview) = self.preview {
+				if buf.len() < width {
+					line.push("   ".repeat(width - buf.len()).into());
+				}
+				line.push("▏".into());
+				(preview)(&mut line, buf);
+			}
+
+			if ansi {
+				write!(out, "{}", ansi_term::ANSIStrings(&line))?;
+			} else {
+				for seg in &line {
+					write!(out, "{}", &**seg)?;
+				}
+			}
+			writeln!(out)?;
+
+			if Some(pos) == len.map(|a| self.start+a) || buf.is_empty() { break; }
+		}
+
+		if self.newline {
+			writeln!(out)?;
+		}
+
+		Ok(())
+	}
+
+	pub fn to_string(self, ansi: bool) -> String {
+		let mut out = Vec::new();
+		self.write_to(&mut out, ansi).unwrap();
+		String::from_utf8(out).unwrap()
+	}
+
+	pub fn to_stdout(self) {
+		let mut out = io::stdout().lock();
+		let mut ansi = std::env::var_os("NO_COLOR").is_none();
+		#[cfg(feature="atty")]
+		{ ansi &= atty::is(atty::Stream::Stdout); }
+		self.write_to(&mut out, ansi).unwrap()
+	}
+
+	pub fn to_stderr(self) {
+		let mut out = io::stderr().lock();
+		let mut ansi = std::env::var_os("NO_COLOR").is_none();
+		#[cfg(feature="atty")]
+		{ ansi &= atty::is(atty::Stream::Stderr); }
+		self.write_to(&mut out, ansi).unwrap()
+	}
+}

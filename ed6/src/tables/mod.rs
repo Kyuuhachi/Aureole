dbf43d1a5635745b1407c09f0570c521a6e90b88
Kyuuhachi <caagr98@gmail.com>
2022-09-20 21:51:10 +0200

    Parse t_orb
    That one's surprisingly complicated. Might benefit from a smallvec.

--- ed6/src/tables/mod.rs
+++ ed6/src/tables/mod.rs
@@ -19,7 +19,75 @@ pub mod bgmtbl;
 pub mod exp;
 pub mod face;
 pub mod name;
+pub mod orb;
 pub mod se;
 pub mod status;
 pub mod town;
 pub mod world;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Element {
+	Earth,
+	Water,
+	Fire,
+	Wind,
+	Time,
+	Space,
+	Mirage,
+}
+
+impl Element {
+	// I want to use TryFrom and From, but Rust's orphan rules don't allow that for Option<Element>.
+
+	pub fn from_u8(v: u8) -> Result<Element, crate::util::CastError> {
+		match v {
+			0 => Ok(Element::Earth),
+			1 => Ok(Element::Water),
+			2 => Ok(Element::Fire),
+			3 => Ok(Element::Wind),
+			4 => Ok(Element::Time),
+			5 => Ok(Element::Space),
+			6 => Ok(Element::Mirage),
+			_ => return Err(crate::util::cast_error::<Element>(v.to_string(), "invalid enum value")),
+		}
+	}
+
+	pub fn from_u8_opt(v: u8) -> Result<Option<Element>, crate::util::CastError> {
+		match v {
+			0 => Ok(None),
+			1 => Ok(Some(Element::Earth)),
+			2 => Ok(Some(Element::Water)),
+			3 => Ok(Some(Element::Fire)),
+			4 => Ok(Some(Element::Wind)),
+			5 => Ok(Some(Element::Time)),
+			6 => Ok(Some(Element::Space)),
+			7 => Ok(Some(Element::Mirage)),
+			_ => return Err(crate::util::cast_error::<Option<Element>>(v.to_string(), "invalid enum value")),
+		}
+	}
+
+	pub fn to_u8(v: Element) -> u8 {
+		match v {
+			Element::Earth => 0,
+			Element::Water => 1,
+			Element::Fire => 2,
+			Element::Wind => 3,
+			Element::Time => 4,
+			Element::Space => 5,
+			Element::Mirage => 6,
+		}
+	}
+
+	pub fn to_u8_opt(v: Option<Element>) -> u8 {
+		match v {
+			None => 0,
+			Some(Element::Earth) => 1,
+			Some(Element::Water) => 2,
+			Some(Element::Fire) => 3,
+			Some(Element::Wind) => 4,
+			Some(Element::Time) => 5,
+			Some(Element::Space) => 6,
+			Some(Element::Mirage) => 7,
+		}
+	}
+}
---
+++ ed6/src/tables/orb.rs
@@ -0,0 +1,97 @@
+use hamu::read::coverage::Coverage;
+use hamu::read::le::*;
+use hamu::write::le::*;
+use crate::archive::Archives;
+use crate::util::*;
+use super::Element;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Orbment {
+	pub slots: Vec<Option<Element>>, // 6 in FC, 7 in SC/3rd
+	pub lines: Vec<Vec<u8>>,
+}
+
+pub fn read(_arcs: &Archives, data: &[u8]) -> Result<Vec<Orbment>, ReadError> {
+	let mut f = Coverage::new(Bytes::new(data));
+	let n = f.clone().u16()? / 2;
+	let mut list = Vec::with_capacity(n as usize);
+
+	let nslots = 6; // 7 in sc/3rd
+	let npad = 1; // 2 in sc/3rd
+
+	for _ in 0..n {
+		let mut g = f.clone().at(f.u16()? as usize)?;
+
+		let mut slots = Vec::with_capacity(nslots);
+		for _ in 0..nslots {
+			slots.push(Element::from_u8_opt(g.u8()?)?);
+		}
+		g.check(&[0;2][..npad])?;
+
+		let nlines = g.u8()?;
+		let mut lines = Vec::with_capacity(nlines as usize);
+		for _ in 0..nlines {
+			lines.push(
+				g.array::<8>()?
+				.into_iter()
+				.take_while(|&a| a != 0xFF)
+				.collect()
+			);
+		}
+		g.check(&[0xFF; 2])?;
+
+
+		list.push(Orbment { slots, lines });
+	}
+
+	f.assert_covered()?;
+	Ok(list)
+}
+
+pub fn write(_arcs: &Archives, list: &[Orbment]) -> Result<Vec<u8>, WriteError> {
+	let mut head = Out::new();
+	let mut body = Out::new();
+	let mut count = Count::new();
+
+	let nslots = 6; // 7 in sc/3rd
+	let npad = 1; // 2 in sc/3rd
+
+	for Orbment { slots, lines } in list {
+		let l = count.next();
+		head.delay_u16(l);
+		body.label(l);
+
+		if slots.len() != nslots {
+			return Err(format!("must be {nslots}").into())
+		}
+		for s in slots {
+			body.u8(Element::to_u8_opt(*s));
+		}
+		body.slice(&[0;2][..npad]);
+
+		body.u8(cast(lines.len())?);
+		for line in lines {
+			if line.len() > 8 {
+				return Err("line cannot be longer than 8".to_owned().into())
+			}
+			let mut buf = [0xFF; 8];
+			buf[..line.len()].copy_from_slice(line);
+			body.array(buf);
+		}
+		body.array([0xFF; 2]);
+	}
+	head.concat(body);
+	Ok(head.finish()?)
+}
+
+#[cfg(test)]
+mod test {
+	use crate::archive::Archives;
+	use crate::util::test::*;
+
+	#[test_case::test_case(&FC; "fc")]
+	fn roundtrip(arc: &Archives) -> Result<(), Error> {
+		check_roundtrip(arc, "t_orb._dt", super::read, super::write)?;
+		Ok(())
+	}
+}

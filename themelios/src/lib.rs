213f2398c60c1497cfb0aca739c50e94dc8b93cd
Kyuuhachi <caagr98@gmail.com>
2022-12-14 22:57:25 +0100

    Move current stuff to themelios-scena
    I plan to move out as much as possible into a separate crate from the
    code parsing, because compiling that macro invocation and huge enum is
    slow.
    
    Next step is to move the testing to a separate crate, so that it can use
    as many dependencies as it needs. After this more things will be moved
    out into separate crates.
    
    Probably the final result will be to have a `common` crate at the bottom
    defining various types, traits, and utils, which is used by `table` and
    `scena` crates, which in turn are used by the top-level `themelios`
    module, which also handles IO, and is the one intended for public use.

--- themelios/src/lib.rs
+++ themelios/src/lib.rs
@@ -1,11 +1,1 @@
-#![feature(error_generic_member_access, provide_any)]
-#![feature(array_try_map, try_trait_v2, never_type, try_trait_v2_residual)]
-#![feature(let_chains)]
-
-pub mod decompress;
-pub mod gamedata;
-pub mod archive;
-pub mod tables;
-pub mod util;
-pub mod scena;
-pub mod text;
+pub use themelios_scena::*;

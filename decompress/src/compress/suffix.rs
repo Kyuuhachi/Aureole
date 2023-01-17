d2e1bb2cf6279009e41668a0b45e98291dc8b851
Kyuuhachi <caagr98@gmail.com>
2023-01-17 20:06:12 +0100

    Use divsufsort
    It's slightly faster, and less code.

--- decompress/src/compress/suffix.rs
+++ decompress/src/compress/suffix.rs
@@ -1,158 +1,6 @@
 // Made according to this guide https://zork.net/~st/jottings/sais.html
 use std::{cmp::Ordering, marker::PhantomData};
 
-trait Value: Ord + Clone + Into<usize> {
-	fn i(&self) -> usize {
-		self.clone().into()
-	}
-}
-impl Value for u8 {}
-impl Value for usize {}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-#[repr(u8)]
-enum Type {
-	S, // smaller
-	L, // larger
-}
-
-fn types(t: &[impl Ord]) -> Vec<Type> { // TODO use bitvec?
-	let mut res = vec![Type::S; t.len() + 1];
-	if res.len() > 1 {
-		res[t.len()-1] = Type::L;
-		for i in (0..res.len()-2).rev() {
-			res[i] = match t[i].cmp(&t[i+1]) {
-				Ordering::Less => Type::S,
-				Ordering::Equal => res[i+1],
-				Ordering::Greater => Type::L,
-			}
-		}
-	}
-	res
-}
-
-#[inline]
-fn is_lms(types: &[Type], pos: usize) -> bool {
-	pos > 0 && types[pos] == Type::S && types[pos-1] == Type::L
-}
-
-#[inline]
-fn buckets<'a, const TAIL: bool>(t: &[impl Value], buckets: &'a mut [usize]) -> &'a mut [usize] {
-	buckets.fill(0);
-	for b in t {
-		buckets[b.i()] += 1;
-	}
-	let mut o = 1;
-	for b in buckets.iter_mut() {
-		o += *b;
-		*b = o - if TAIL { 0 } else { *b };
-	}
-	buckets
-}
-
-fn make_array(t: &[impl Value], scratch: &mut [usize], sa: &mut [usize]) {
-	let types = &types(t);
-
-	lms_sort(t, types, scratch, sa,
-		(0..t.len()).filter(|&i| is_lms(types, i))
-	);
-
-	let (names, n_names) = get_names(t, types, sa);
-
-	let (summary_offsets, t1): (Vec<_>, Vec<_>) = names.into_iter()
-		.enumerate()
-		.filter(|a| a.1 != usize::MAX)
-		.unzip();
-
-	let mut sa1 = vec![usize::MAX; t1.len()+1];
-	if t1.len() == n_names {
-		sa1[0] = t1.len();
-		for (i, &c) in t1.iter().enumerate() {
-			sa1[c+1] = i
-		}
-	} else {
-		make_array(&t1, &mut vec![0; n_names], &mut sa1);
-	};
-
-	lms_sort(t, types, scratch, sa,
-		sa1[2..].iter().map(|&i| summary_offsets[i])
-	);
-}
-
-fn lms_sort(
-	t: &[impl Value],
-	types: &[Type],
-	scratch: &mut [usize],
-	sa: &mut [usize],
-	ix: impl DoubleEndedIterator<Item=usize>,
-) {
-	sa.fill(usize::MAX);
-	sa[0] = t.len();
-
-	let tails = buckets::<true>(t, scratch);
-	for i in ix.rev() {
-		debug_assert!(i > 0);
-		let v = &mut tails[t[i].i()];
-		*v -= 1;
-		sa[*v] = i;
-	}
-
-	let heads = buckets::<false>(t, scratch);
-	for i in 0..sa.len() {
-		if sa[i] != usize::MAX && sa[i] != 0 && types[sa[i]-1] == Type::L {
-			let v = &mut heads[t[sa[i]-1].i()];
-			debug_assert!(*v > i);
-			sa[*v] = sa[i] - 1;
-			*v += 1;
-		}
-	}
-
-	let tails = buckets::<true>(t, scratch);
-	for i in (0..sa.len()).rev() {
-		if sa[i] != usize::MAX && sa[i] != 0 && types[sa[i]-1] == Type::S {
-			let v = &mut tails[t[sa[i]-1].i()];
-			debug_assert!(*v <= i);
-			*v -= 1;
-			sa[*v] = sa[i] - 1;
-		}
-	}
-}
-
-fn get_names(t: &[impl Value], types: &[Type], sa: &[usize]) -> (Vec<usize>, usize) {
-	let mut names = vec![usize::MAX; t.len()+1];
-	let mut cur_name = 0;
-	names[sa[0]] = cur_name;
-	let mut last_offset = sa[0];
-	for &offset in &sa[1..] {
-		if is_lms(types, offset) {
-			if !lms_equal(t, types, last_offset, offset) {
-				cur_name += 1
-			}
-			last_offset = offset;
-			names[offset] = cur_name;
-		}
-	}
-	(names, cur_name+1)
-}
-
-fn lms_equal(t: &[impl Value], types: &[Type], a: usize, b: usize) -> bool {
-	if a == t.len() || b == t.len() {
-		return a == b
-	}
-	let mut i = 0;
-	loop {
-		if t[a+i] != t[b+i] {
-			return false
-		}
-		i += 1;
-		let al = is_lms(types, a+i);
-		let bl = is_lms(types, b+i);
-		if al || bl {
-			return al && bl
-		}
-	}
-}
-
 #[derive(Debug)]
 pub struct SuffixArray<'a, T: ?Sized> {
 	text: &'a [u8],
@@ -237,9 +85,12 @@ macro_rules! common {
 impl<'a, T: Text + ?Sized> SuffixArray<'a, T> {
 	pub fn new(text: &'a T) -> Self {
 		let text = T::as_bytes(text);
-		let mut sa = vec![0; text.len()+1];
-		make_array(text, &mut [0; 256], &mut sa);
-		sa.retain(|a| T::is_index(text, *a));
+		let mut sa = vec![0; text.len()];
+		divsufsort::sort_in_place(text, &mut sa);
+		let sa = sa.into_iter()
+			.map(|a| a as usize)
+			.filter(|a| T::is_index(text, *a))
+			.collect();
 		SuffixArray { text, sa, _ph: PhantomData }
 	}
 

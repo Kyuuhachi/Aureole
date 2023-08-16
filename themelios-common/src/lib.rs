9a581c1ea8ab06caddd7ef4eee0ae938ac5f790f
Kyuuhachi <caagr98@gmail.com>
2023-08-16 09:53:22 +0200

    Update thiserror

--- themelios-common/src/lib.rs
+++ themelios-common/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(error_generic_member_access, provide_any)]
+#![feature(error_generic_member_access)]
 #![feature(array_try_map, try_trait_v2, never_type, try_trait_v2_residual)]
 #![feature(let_chains)]
 #![feature(decl_macro)]
